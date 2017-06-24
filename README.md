fae8f96aecd077a4a02b9f6daba5e7953d636c9b
offtechnologies <artur.smorga@mac.com>
2017-06-24 15:55:00 +0100

    Added initial setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_redmine_demo
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef_redmine_demo
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
