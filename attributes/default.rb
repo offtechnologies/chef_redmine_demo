9dd8b51ad832b7152d24d93a3f3c98ae45d8d34b
offtechnologies <artur.smorga@mac.com>
2017-06-24 16:37:11 +0100

    Added new recipe mysql_setup

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,12 @@ default['system_setup']['timezone']      = 'Europe/Berlin'
 default['system_setup']['service_name']  = 'redmine'
 default['system_setup']['dest_dir']      = "/srv/#{node['system_setup']['service_name']}"
 default['system_setup']['host_ip']       = '127.0.0.1'
+
+# user_setup
+default['user_setup']['user']            = node['system_setup']['service_name']
+
+# mysql_setup
+default['mysql_setup']['dbhost']         = '127.0.0.1'
+default['mysql_setup']['dbname']         = node['system_setup']['service_name']
+default['mysql_setup']['dbadmin']        = node['user_setup']['user']
+default['mysql_setup']['dbadapter']      = 'mysql2'
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,5 @@
 # Copyright:: 2017, OFF Technologies, All Rights Reserved.
 # setup apt, basic packages, etc
 include_recipe 'chef_redmine_demo::system_setup'
+# install and setup mysql
+include_recipe 'chef_redmine_demo::mysql_setup'
